b3a1be3cb0171e40520511cab0ea44a2c7699799
Sebastian Utbult <sebastian.utbult@awardit.com>
2024-09-25 10:46:40 +0200

    better phpcs pipeline

--- src/Tags.php
+++ src/Tags.php
@@ -14,7 +14,11 @@ class Tags
     public static function getTagsForLarp(Larp $larp): array
     {
         $db = new DB();
-        $tags = $db->getAll("SELECT tags.* FROM tags, larp_has_tags WHERE larp_has_tags.tag_id = tags.id AND larp_has_tags.larp_id = " . (int)$larp->id . " ORDER BY tag ASC");
+        $tags = $db->getAll(
+            "SELECT tags.* FROM tags, larp_has_tags
+            WHERE larp_has_tags.tag_id = tags.id AND larp_has_tags.larp_id = "
+            . (int)$larp->id . " ORDER BY tag ASC"
+        );
         return $tags;
     }
 }
